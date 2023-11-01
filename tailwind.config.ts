8028063e4458a02e961fb6a71f4924caa429ee72
tomoyo519 <tomoyo519@naver.com>
2023-11-02 00:19:52 +0900

    로그인까지

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
 import type { Config } from 'tailwindcss'
-
+import { join } from 'path'
 const config: Config = {
   content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
+    join(__dirname, './src/pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, './src/pages/*.{js,ts,jsx,tsx}'),
+    join(__dirname, './src/components/**/*.{js,ts,jsx,tsx)'),
+    join(__dirname, './src/app/*.{js,ts,jsx,tsx)'),
   ],
   theme: {
     extend: {
